ec4ae5b999dceef29330222a2790c4ed18b329ac
SiddharthManjul <manjulsiddharth09@gmail.com>
2026-08-25 08:28:36 +0530

    Export ModalCards from the package entry.

---
+++ src/components/ModalCards/index.ts
@@ -0,0 +1,10 @@
+export { ModalCards, default } from "./ModalCards";
+export { GridCard } from "./GridCard";
+export { Modal } from "./Modal";
+export { defaultCards, SPEED_PRESETS } from "./types";
+export type {
+  CardData,
+  ModalCardsProps,
+  AnimationSpeed,
+  AnimationVariant,
+} from "./types";
