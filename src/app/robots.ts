a9012517b0e3ee2be1e1966512bb4098542eeb56
SiddharthManjul <manjulsiddharth09@gmail.com>
2026-09-15 22:45:21 +0530

    disallow admin from robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/api/",
+      disallow: ["/api/", "/admin", "/og-preview"],
     },
     sitemap: `${SITE_ORIGIN}/sitemap.xml`,
     host: SITE_ORIGIN,
