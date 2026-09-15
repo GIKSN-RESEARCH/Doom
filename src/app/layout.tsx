3bb66583709b7dc57795be810009eecd0333626c
SiddharthManjul <manjulsiddharth09@gmail.com>
2026-09-15 22:45:21 +0530

    root layout children typing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,7 +105,11 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const gaId = process.env.NEXT_PUBLIC_GA_ID;
 
   return (
